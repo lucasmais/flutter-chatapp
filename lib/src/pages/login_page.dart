f663b67b16f291498ddd384a35e1184e22bc6ce9
lucasmais <lucas@mais.es>
2020-12-04 11:16:01 +0100

    Pantalla Login y Widgets

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,4 +1,7 @@
+import 'package:chat/src/widgets/boton_azul.dart';
 import 'package:chat/src/widgets/custom_input.dart';
+import 'package:chat/src/widgets/labels_widget.dart';
+import 'package:chat/src/widgets/logo_widget.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -9,40 +12,21 @@ class LoginPage extends StatelessWidget {
         backgroundColor: Color(0xffF2F2F2),
         body: SingleChildScrollView(
           physics: BouncingScrollPhysics(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              _Logo(),
-              _Form(),
-              _Labels(),
-              Text('Términos y condiciones de uso',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w200,
-                  )),
-            ],
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LogoWidget(),
+                _Form(),
+                LabelsWidget(),
+                Text('Términos y condiciones de uso',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w200,
+                    )),
+              ],
+            ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class _Logo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: EdgeInsets.only(top: 50),
-        width: 170,
-        child: Column(
-          children: [
-            Image(image: AssetImage('assets/tag-logo.png')),
-            SizedBox(height: 20),
-            Text(
-              'Messenger',
-              style: TextStyle(fontSize: 30),
-            )
-          ],
         ),
       ),
     );
@@ -55,6 +39,8 @@ class _Form extends StatefulWidget {
 }
 
 class __FormState extends State<_Form> {
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -66,42 +52,17 @@ class __FormState extends State<_Form> {
             icon: Icons.mail_outline,
             placeolder: 'Email',
             keyboardType: TextInputType.emailAddress,
-            textController: null,
+            textController: emailController,
           ),
           CustomInput(
-            icon: Icons.vpn_key,
+            icon: Icons.lock_outline,
             placeolder: 'Password',
-            textController: null,
+            textController: passController,
             isPassword: true,
           ),
-          //TODO: crear boton
-          // RaisedButton(onPressed: () {}),
-        ],
-      ),
-    );
-  }
-}
-
-class _Labels extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text(
-            '¿No tienes una cuenta?',
-            style: TextStyle(
-                color: Colors.black54,
-                fontSize: 15,
-                fontWeight: FontWeight.w300),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Crea una ahora!',
-            style: TextStyle(
-                color: Colors.blue[600],
-                fontSize: 18,
-                fontWeight: FontWeight.bold),
+          BotonAzul(
+            text: 'Ingrese',
+            onPressed: () {},
           ),
         ],
       ),
---
+++ lib/src/widgets/boton_azul.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class BotonAzul extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+
+  const BotonAzul({
+    @required this.text,
+    @required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: this.onPressed,
+      elevation: 2,
+      highlightElevation: 5,
+      color: Colors.blue,
+      shape: StadiumBorder(),
+      child: Container(
+        width: double.infinity,
+        height: 55,
+        child: Center(
+          child: Text(
+            this.text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 17,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
